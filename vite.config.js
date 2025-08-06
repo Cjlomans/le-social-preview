4d1305000dee186c6ef23ccc502d470f53b0f3b8
Chris Lomans <cjlomans@gmail.com>
2025-08-06 22:13:48 +1000

    fix: deploy fresh build to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/le-social-preview/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
 
+// Tell Vite to prefix every link with /le-social-preview/
+export default defineConfig({
+  base: '/le-social-preview/',
+  plugins: [react()],
+})
